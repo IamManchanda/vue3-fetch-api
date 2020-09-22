8135061e1c500a1990e208029739a36000d43789
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-23 01:08:14 +0530

    code changes

---
+++ src/components/fetch-data.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{ count }}</div>
+</template>
+
+<script>
+import { reactive, toRefs } from "vue";
+
+export default {
+  name: "FetchData",
+  setup() {
+    const state = reactive({
+      count: 0,
+    });
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <fetch-data />
+  </div>
+</template>
+
+<script>
+import FetchData from "@/components/fetch-data";
+export default {
+  components: { FetchData },
+};
+</script>
